5778d75d05077cf8396563aeb3ffce9f927b3b45
AnnaBsoares <RM99814@fiap.com.br>
2023-06-03 19:49:58 -0300

    alteraçao na formataçao das paginas

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let contador = 1;
 
 //cada vzs q checked vai contar 1
 //uma função ocorra a cada espaço de tempo (setinterval)
-setInterval( function(){
+setInterval(function () {
     //vai dar um checked para cada elemento
     document.getElementById('slide' + contador).checked = true;
     contador++;
     /*reinicia - para n ficar em loop - condicional*/
-    if(counter > 5 ) {
+    if (counter > 5) {
         contador = 1;
     }
-//em 3milisegundos essa função vai ocorrer
-}, 2500 );
+    //em 3milisegundos essa função vai ocorrer
+}, 2500);
 
 const wrapper = document.querySelector('.wrapper');
 const botaoLogin = document.getElementById('botaoLogin');
@@ -21,9 +21,9 @@ const loginLink = document.querySelector('.login-link');
 
 botaoLogin.addEventListener('click', () => {
     wrapper.classList.add('active-botao');
-    document.getElementById('wrapper').style.transform='scale(1)';
+    document.getElementById('wrapper').style.transform = 'scale(1)';
 });
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
-    
+
 });
